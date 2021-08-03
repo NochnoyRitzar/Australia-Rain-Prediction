aaf60710ebfd06a8e77f85e734144f0eb8377a0f
Jenia Gerasimov <yevhen.gerasimov@gmail.com>
2021-08-03 22:22:36 +0300

    everything done, only training left

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import yaml
 import logging
 import re
+import time
+
 
 import pandas as pd
 import numpy as np
@@ -11,14 +13,20 @@ import matplotlib.pyplot as plt
 import category_encoders as ce
 from imblearn.over_sampling import SMOTE
 
-from sklearn.feature_selection import SequentialFeatureSelector
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.feature_selection import f_classif, f_regression
+from sklearn.feature_selection import SelectKBest
+from sklearn.model_selection import HalvingRandomSearchCV, train_test_split
 from sklearn.preprocessing import RobustScaler, MinMaxScaler
-from sklearn.linear_model import LogisticRegression
+#Import models
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, precision_score, \
-    recall_score, f1_score, classification_report, ConfusionMatrixDisplay
+    recall_score, f1_score, classification_report, ConfusionMatrixDisplay, r2_score
 
 def load_dataset(csv):
     """
@@ -38,8 +46,7 @@ def optimize_data(df):
     :return: Optimized DataFrame with less columns
     """
     #Changing column data type to category
-    #cat_cols = df.select_dtypes(include="object")
-    #df[cat_cols] = df[cat_cols].astype("category")
+    df.loc[:, config.get("object_cols")] = df.loc[:, config.get("object_cols")].astype("category")
 
     #Dropping uninformative and redundant columns
     df.drop(columns=config.get("cols_to_drop"), axis=1, inplace=True)
@@ -165,37 +172,162 @@ def select_features(x_train, y_train):
     Selecting best features with SelectKBest
     :param x_train: train features
     :param y_train: train target
-    :return: selected train features
+    :return: train sets for regression and classification models
     """
+    #Select features for regression model
+    select_k_best = SelectKBest(score_func=f_regression, k=8).fit(x_train, y_train)
+    best_cols = select_k_best.get_support(indices=True)
+    x_train_regr = x_train.iloc[:, best_cols]
 
-    knn = KNeighborsClassifier()
-    sfs = SequentialFeatureSelector(knn, n_features_to_select=8)
-    sfs.fit(x_train, y_train)
-    print("test")
-    best_cols = sfs.get_support(indices=True)
-    x_train = x_train.iloc[:, best_cols]
-    x_train.head()
-
-    # select_k_best = SelectKBest(score_func=f_classif, k=8).fit(x_train, y_train)
-    # x_train = select_k_best.transform(x_train)
-    # best_cols = select_k_best.get_support(indices=True)
-    # x_train = x_train.iloc[:, best_cols]
-
-    return x_train
+    #Select features for classification model
+    select_k_best = SelectKBest(score_func=f_classif, k=8).fit(x_train, y_train)
+    best_cols = select_k_best.get_support(indices=True)
+    x_train_classif = x_train.iloc[:, best_cols]
+    print("Best features selected")
+    return x_train_regr, x_train_classif
 
 
-def tune_models(x_train, y_train):
+def tune_reg_models(x_train_regr, y_train):
     """
-    Training various ML models and tuning their parameters
-    :param x_train: training features
-    :param y_train: training target
+    Tuning hyper-parameters of regression estimators and selecting best estimators
+    :param x_train_regr: train data (features) for regression models
+    :param y_train: train data (target)
+    :return: dictionary of best regression estimators
+    """
+    regr_models = {"lin_reg":LinearRegression(),
+                  "log_reg":LogisticRegression()}
+    regr_estimators = {}
+    logging.info("Tuning regression models...\n")
+    for estimator_name, estimator in regr_models.items():
+        start = time.time()
+        clf = HalvingRandomSearchCV(estimator=estimator,
+                                    param_grid=config.get(estimator_name),
+                                    scoring="r2",
+                                    cv=5)
+        clf.fit(x_train_regr, y_train)
+        stop = time.time()
+        logging.info(f"Training time: {stop - start}s")
+        best_score = clf.best_score_
+        regr_estimators[estimator_name] = clf.best_estimator_
+        logging.info("{} best score: {}".format(estimator, best_score))
+    print("Regression models tuned")
+    return regr_estimators
+
+
+def tune_classif_models(x_train_classif, y_train):
+    """
+    Tuning hyper-parameters of classification estimators and selecting best estimators
+    :param x_train_classif: train data (features) for classification models
+    :param y_train: train data (target)
+    :return: dictionary of best classification estimators
+    """
+    classif_models = {"svm":SVC(),
+                      "knn":KNeighborsClassifier(),
+                      "decision_tree":DecisionTreeClassifier(),
+                      "random_forest":RandomForestClassifier(),
+                      "mlp":MLPClassifier()}
+
+    logging.info("Tuning regression models...\n")
+    classif_estimators = {}
+    for estimator_name, estimator in classif_models.items():
+        start = time.time()
+        clf = HalvingRandomSearchCV(estimator=estimator,
+                                    param_grid=config.get(estimator_name),
+                                    scoring="roc_auc",
+                                    cv=5)
+        clf.fit(x_train_classif, y_train)
+        stop = time.time()
+        logging.info(f"Training time: {stop - start}s")
+        best_score = clf.best_score_
+        classif_estimators[estimator_name] = clf.best_estimator_
+        logging.info("{} best score: {}".format(estimator, best_score))
+    print("Classification models tuned")
+    return classif_estimators
+
+
+def evaluate_models(regr_estimators, classif_estimators,
+                    x_train_regr, x_train_classif, x_test, y_train, y_test):
+    """
+    Retraining best ML models and evaluating them
+    :param regr_estimators: dictionary of regression models
+    :param classif_estimators: dictionary of classification models
+    :param x_train_regr: regression train data (features)
+    :param x_train_classif: classification train data (features)
+    :param x_test: test data (features)
+    :param y_train: train data (target)
+    :param y_test: test data (target)
     :return:
     """
+    evaluation_data = {}
 
-    pass
+    logging.info("Evaluating regression models: \n")
+    for model_name, estimator in regr_estimators.items():
+        estimator.fit(x_train_regr, y_train)
+        predict = estimator.predict(x_test)
+        predict_probability = estimator.predict_proba(x_test)[:, 1]
+
+        logging.info("{}: \n".format(model_name))
+        accuracy = accuracy_score(y_test, predict)
+        precision = precision_score(y_test, predict)
+        recall = recall_score(y_test, predict)
+        cm = confusion_matrix(y_test, predict)
+        cr = classification_report(y_test, predict)
+        f1 = f1_score(y_test, predict)
+        roc_auc = roc_auc_score(y_test, predict_probability)
+        logging.info("Accuracy: {}".format(accuracy))
+        logging.info("Precision: {}".format(precision))
+        logging.info("Recall: {}".format(recall))
+        logging.info("Confusion matrix: \n{}".format(cm))
+        logging.info("Classification report: \n{}".format(cr))
+        logging.info("F1 score: {}".format(f1))
+        logging.info("ROC AUC score: {}\n".format(roc_auc))
+        evaluation_data[model_name] = [accuracy, f1, roc_auc]
+
+        fig = plt.figure(figsize=(8, 8))
+        sns.heatmap(cm, cmap='Blues', annot=True, fmt='d', linewidths=5, cbar=False,
+                    annot_kws={'fontsize': 15}, yticklabels=['0', '1'],
+                    xticklabels=['Predict 0', 'Predict 1'])
+        fig.savefig("results/{}_confusion_matrix.png".format(model_name))
+
+    logging.info("Evaluating classification models: \n")
+    for model_name, estimator in classif_estimators.items():
+        estimator.fit(x_train_classif, y_train)
+        predict = estimator.predict(x_test)
+        predict_probability = estimator.predict_proba(x_test)[:, 1]
+
+        logging.info("{}: \n".format(model_name))
+        accuracy = accuracy_score(y_test, predict)
+        precision = precision_score(y_test, predict)
+        recall = recall_score(y_test, predict)
+        cm = confusion_matrix(y_test, predict)
+        cr = classification_report(y_test, predict)
+        f1 = f1_score(y_test, predict)
+        roc_auc = roc_auc_score(y_test, predict_probability)
+        logging.info("Accuracy: {}".format(accuracy))
+        logging.info("Precision: {}".format(precision))
+        logging.info("Recall: {}".format(recall))
+        logging.info("Confusion matrix: \n{}".format(cm))
+        logging.info("Classification report: \n{}".format(cr))
+        logging.info("F1 score: {}".format(f1))
+        logging.info("ROC AUC score: {}\n".format(roc_auc))
+        evaluation_data[model_name] = [accuracy, f1, roc_auc]
+
+        fig = plt.figure(figsize=(8, 8))
+        sns.heatmap(cm, cmap='Blues', annot=True, fmt='d', linewidths=5, cbar=False,
+                    annot_kws={'fontsize': 15}, yticklabels=['0', '1'],
+                    xticklabels=['Predict 0', 'Predict 1'])
+        fig.savefig("results/{}_confusion_matrix.png".format(model_name))
+
+    df_evaluation = pd.DataFrame.from_dict(evaluation_data, orient="index",
+                                           columns=["accuracy", "f1_score", "roc_auc_score"])
+    df_evaluation.to_csv("results/evaluation.csv")
+    print("Work complete!")
+
 
 with open("Config/config.yaml", 'r') as file:
     config = yaml.safe_load(file)
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO, filename="Results/logfile")
 
 df  = load_dataset("weatherAUS.csv")
 df = optimize_data(df)
@@ -204,4 +336,8 @@ df = scale_cat_data(df)
 df = handle_outliers(df)
 x_train, x_test, y_train, y_test = split_sample_data(df)
 x_train, x_test = scale_data(x_train, x_test)
-x_train = select_features(x_train, y_train)
+x_train_regr, x_train_classif  = select_features(x_train, y_train)
+regr_estimators = tune_reg_models(x_train_regr, y_train)
+classif_estimators = tune_classif_models(x_train_classif, y_train)
+evaluate_models(regr_estimators, classif_estimators,
+                x_train_regr, x_train_classif, x_test, y_train, y_test)
